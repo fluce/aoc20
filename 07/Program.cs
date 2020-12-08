8c173706aae7ee53f524201aacb853564224ad3f
Frédéric Luce <frederic.luce@gmail.com>
2020-12-08 21:56:36 +0100

    Day 8

---
+++ 07/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args.FirstOrDefault()??"input.txt").ToArray();
+
+            var regex=new Regex(@"^(?<outerColor>\S+ \S+) bags contain (?:(?<none>no other bags)|(?:(?<innerCount>\d+) (?<innerColor>\S+ \S+) bags?)(?:, (?<innerCount>\d+) (?<innerColor>\S+ \S+) bags?)*).$");
+
+            var index=new Dictionary<string,Item>();
+
+            foreach(var line in lines) {
+                var m=regex.Match(line);
+                var item=new Item(  outerColor:m.Groups["outerColor"].Value, 
+                                    inner:      m.Groups["innerCount"].Captures.Cast<Capture>()
+                                        .Zip(   m.Groups["innerColor"].Captures.Cast<Capture>(),
+                                                (a,b)=>new Child(color:b.Value,count:int.Parse(a.Value))
+                                        ).ToList()
+                                );
+                Console.WriteLine($"outer: {item.outerColor}, inner: {string.Join("-",item.inner.Select(x=>$"{x.count} {x.color}"))}");
+                index[item.outerColor]=item;
+            }
+
+            var count=index.Where(x=>x.Key!="shiny gold").Where(x=>GetAllDescendants(x.Value,index).Any(i=>i.color=="shiny gold")).Count();
+            Console.WriteLine($"Count = {count}");
+
+            int innerBagCount=CountDescendants(index["shiny gold"],index)-1;
+            Console.WriteLine($"Bag Count = {innerBagCount}");
+        }
+
+        static IEnumerable<Child> GetAllDescendants(Item item, Dictionary<string,Item> index, string prefix="") 
+            { 
+//                Console.WriteLine($"{prefix}Item {item.outerColor}");
+                foreach (var i in item.inner) {
+//                    Console.WriteLine($"{prefix}Child {i.color} {i.count}");
+                    yield return i;
+                    foreach (var desc in GetAllDescendants(index[i.color],index,prefix+"  "))
+                        yield return desc;
+                }
+            }
+
+        static int CountDescendants(Item item, Dictionary<string,Item> index, string prefix="") 
+            { 
+//                Console.WriteLine($"{prefix}Item {item.outerColor}");
+                int total=1;
+                foreach (var i in item.inner) {
+//                    Console.WriteLine($"{prefix}Child {i.color} {i.count}");
+                    var desccount=CountDescendants(index[i.color],index,prefix+"  ");
+//                    Console.WriteLine($"{prefix}Child total : {desccount*i.count}");
+                    total+=desccount*i.count;
+                }
+//                Console.WriteLine($"{prefix}Item total {total}");
+                return total;
+            }
+
+    }
+
+    record Item(
+        string outerColor,
+        List<Child> inner
+    );
+
+    record Child(
+        string color,
+        int count
+    );
+}
---
+++ 08/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _08
+{
+    record Instruction
+    {
+        public string Operator;
+
+        public string Operand;
+
+        public int OperandAsInt=>int.Parse(Operand);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args.FirstOrDefault()??"input.txt").ToArray();
+
+            var regex=new Regex(@"^(?<token>\w+)\s+(?<value>[+-]*\d+)$");
+
+            var program=lines.Select(x=>regex.Match(x)).Select(x=>new Instruction { Operator=x.Groups["token"].Value, Operand=x.Groups["value"].Value }).ToList();
+
+            var (result,ip)=Run(program);
+            Console.WriteLine($"Accumulator {result}");
+
+            for(int i=0;i<program.Count;i++)            
+            {
+                var backup=program[i];
+                if (program[i].Operator=="nop") program[i]=backup with { Operator="jmp" };
+                else
+                    if (program[i].Operator=="jmp") program[i]=backup with { Operator="nop" };
+                    else
+                        continue;
+
+                Console.WriteLine($"Changing {i} {backup}=>{program[i]}");
+                
+                (result,ip)=Run(program);
+                if (ip==program.Count)
+                {
+                    Console.WriteLine($"{i} {backup} => Accumulator {result}");
+                    break;
+                }
+
+                program[i]=backup;
+            }
+
+
+
+        }
+
+        static (int,int) Run(List<Instruction> program)
+        {
+            int accumulator=0;
+            var counters=new int[program.Count];
+            var instructionPointer=0;
+
+            while (true)
+            {
+                var instruction=program[instructionPointer];
+                Console.WriteLine($"IP={instructionPointer} {instruction.Operator} {instruction.OperandAsInt}");
+                
+                if (counters[instructionPointer]==1) break;
+                
+                counters[instructionPointer]++;
+
+                switch(instruction.Operator)
+                {
+                    case "nop": 
+                        instructionPointer++;
+                        break;
+                    case "acc":
+                        accumulator+=instruction.OperandAsInt;
+                        instructionPointer++;
+                        break;
+                    case "jmp":
+                        instructionPointer+=instruction.OperandAsInt;
+                        break;
+                    default:
+                        throw new Exception("Invalid operator");
+                }
+                if (instructionPointer>=program.Count) break;
+            }
+            return (accumulator,instructionPointer);
+        }
+    }
+}
