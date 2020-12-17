075fdbd32176d85ad2c0d952ccbfa1436e461c0f
Frédéric Luce <frederic.luce@gmail.com>
2020-12-17 23:39:42 +0100

    Day 16 & Day 17

---
+++ 16/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _16
+{
+    class Program
+    {
+        static Regex regex=new Regex(@"^(?<field>[^:]+): ((?<int1>\d+)-(?<int2>\d+))( or ((?<int1>\d+)-(?<int2>\d+)))*$");
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args.FirstOrDefault() ?? "input.txt").ToArray();
+
+            int part=0;
+            Dictionary<string,List<(int,int)>> dico=new Dictionary<string, List<(int, int)>>();
+
+            List<int> myticket=null;
+            List<List<int>> nearbyTickets=new List<List<int>>();
+
+            foreach(var l in lines)
+            {
+                if (l.Length==0) part++;
+                else {
+
+                    switch (part)
+                    {
+                        case 0:
+                            var m=regex.Match(l);
+                            var field=m.Groups["field"].Value;
+                            var intervals=m.Groups["int1"].Captures.OfType<Capture>().Zip(m.Groups["int2"].Captures.OfType<Capture>(),(i1,i2)=>(int.Parse(i1.Value),int.Parse(i2.Value)));
+
+                            dico[field]=intervals.ToList();
+
+                            break;
+                        case 1:
+                            if (!l.StartsWith("your")) {
+                                myticket=l.Split(',').Select(int.Parse).ToList();
+                            }
+                            break;
+                        case 2:
+                            if (!l.StartsWith("nearby")) {
+                                nearbyTickets.Add(l.Split(',').Select(int.Parse).ToList());
+                            }
+                            break;
+                    }
+
+                }
+            }
+
+            var allValidIntervals=dico.Values.SelectMany(x=>x).ToList();
+            Console.WriteLine($"All valid intervals: {string.Join(",",allValidIntervals.Select(x=>x.ToString()))}");
+
+            int sum=0;
+            List<List<int>> maybeValidTickets=new List<List<int>>();
+            foreach(var t in nearbyTickets)
+            {
+                var certainlyInvalidFields=t.Where(x=>!allValidIntervals.Any(interval=>x>=interval.Item1 && x<=interval.Item2)).ToList();
+                if (certainlyInvalidFields.Count>0) {
+                    Console.WriteLine($"Invalid fields : {string.Join(" - ",certainlyInvalidFields.Select(x=>x.ToString()))}");
+                    sum+=certainlyInvalidFields.Sum();
+                } else {
+                    maybeValidTickets.Add(t);
+                }
+            }
+            Console.WriteLine($"Ticket scanning error rate : {sum}");
+
+            var indexes=Enumerable.Range(0,myticket.Count).ToList();
+            bool foundSomething=true;
+            
+            long result=1;
+
+            while (indexes.Count>0 && foundSomething) {
+                foundSomething=false;
+                List<int> needRemoval=new List<int>();
+                foreach (var (idx,allValues) in indexes.Select(x=>(x,maybeValidTickets.Select(f=>f[x]).ToList())))
+                {
+                    var fields=dico.Where(x=>allValues.All(y=>x.Value.Any(z=>z.Item1<=y && z.Item2>=y))).ToList();
+                    if (fields.Count==1) {
+                        var fieldName=fields[0].Key;
+                        Console.WriteLine($"{idx}: {fieldName}");
+                        dico.Remove(fieldName);
+                        needRemoval.Add(idx);
+                        foundSomething=true;
+                        if (fieldName.StartsWith("departure")) {
+                            result*=myticket[idx];
+                        }
+                    } /*else {
+                        Console.WriteLine($"{idx}: error : {string.Join(",",fields.Select(x=>x.Key))}");
+                    }*/
+                }
+                indexes.RemoveAll(x=>needRemoval.Contains(x));
+            }
+
+            Console.WriteLine($"Result : {result}");
+
+        }
+    }
+}
---
+++ 17/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace _17
+{
+    public record Coord(int x,int y, int z, int w);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args.FirstOrDefault() ?? "input.txt").ToArray();
+
+            var space=new Dictionary<Coord,char>();
+            Coord min=new Coord(0,0,0,0);
+            Coord max=new Coord(lines[0].Length-1,lines.Length-1,0,0);
+
+            for(int y=0;y<lines.Length;y++)
+                for(int x=0;x<lines[y].Length;x++)
+                    if (lines[y][x]=='#') space[new Coord(x,y,0,0)]=lines[y][x];
+
+            Display(min,max,space);
+
+            var l=new[] { -1,0,1 };
+            neighbours=l.SelectMany(y=>l,(x,y)=>(x,y))
+                        .SelectMany(z=>l,(a,z)=>(a.x,a.y,z))
+                        .SelectMany(w=>l,(a,w)=>(a.x,a.y,a.z,w))
+                        .Where(c=>!(c.x==0&&c.y==0&&c.z==0&&c.w==0)).Select(c=>new Coord(c.x,c.y,c.z,c.w)).ToArray();
+
+            Coord curmin=new Coord(min.x,min.y,min.z,min.w);
+            Coord curmax=new Coord(max.x,max.y,max.z,max.w);
+            Dictionary<Coord,char> curspace=space;
+            for (int i=1;i<=6;i++) {
+                Console.WriteLine($"Iteration {i}");
+                curspace=Iterate(ref curmin, ref curmax, curspace);
+                Display(curmin,curmax,curspace);
+            }
+            Console.WriteLine($"Total={curspace.Count(x=>x.Value=='#')}");
+
+            curmin=new Coord(min.x,min.y,min.z,min.w);
+            curmax=new Coord(max.x,max.y,max.z,max.w);
+            curspace=space;
+            for (int i=1;i<=6;i++) {
+                Console.WriteLine($"Iteration {i}");
+                curspace=Iterate4(ref curmin, ref curmax, curspace);
+                //Display(curmin,curmax,curspace);
+            }
+            Console.WriteLine($"Total 4={curspace.Count(x=>x.Value=='#')}");
+
+        }
+
+        static Coord[] neighbours;
+
+        static Dictionary<Coord,char> Iterate(ref Coord min, ref Coord max, Dictionary<Coord,char> space)
+        {
+            var space2=new Dictionary<Coord,char>();
+            for(int z=min.z-1;z<=max.z+1;z++) 
+                for(int y=min.y-1;y<=max.y+1;y++) 
+                    for(int x=min.x-1;x<=max.x+1;x++) 
+                    {
+                        var c=new Coord(x,y,z,0);
+                        var n=neighbours.Count(n=>space.TryGetValue(new Coord(n.x+x,n.y+y,n.z+z,0),out var cc) && cc=='#');
+                        var active=space.TryGetValue(c,out var cc) && cc=='#';
+                        if ( (active && (n==2 || n==3)) || (!active && n==3) )
+                        {
+                            if (c.x<min.x) min=min with {x=c.x};
+                            if (c.y<min.y) min=min with {y=c.y};
+                            if (c.z<min.z) min=min with {z=c.z};
+                            if (c.x>max.x) max=max with {x=c.x};
+                            if (c.y>max.y) max=max with {y=c.y};
+                            if (c.z>max.z) max=max with {z=c.z};
+                            space2[c]='#';
+                        }
+                    }
+            return space2;
+        }
+
+        static Dictionary<Coord,char> Iterate4(ref Coord min, ref Coord max, Dictionary<Coord,char> space)
+        {
+            var space2=new Dictionary<Coord,char>();
+            for(int w=min.w-1;w<=max.w+1;w++) 
+                for(int z=min.z-1;z<=max.z+1;z++) 
+                    for(int y=min.y-1;y<=max.y+1;y++) 
+                        for(int x=min.x-1;x<=max.x+1;x++) 
+                        {
+                            var c=new Coord(x,y,z,w);
+                            var n=neighbours.Count(n=>space.TryGetValue(new Coord(n.x+x,n.y+y,n.z+z,n.w+w),out var cc) && cc=='#');
+                            var active=space.TryGetValue(c,out var cc) && cc=='#';
+                            if ( (active && (n==2 || n==3)) || (!active && n==3) )
+                            {
+                                if (c.x<min.x) min=min with {x=c.x};
+                                if (c.y<min.y) min=min with {y=c.y};
+                                if (c.z<min.z) min=min with {z=c.z};
+                                if (c.w<min.w) min=min with {w=c.w};
+                                if (c.x>max.x) max=max with {x=c.x};
+                                if (c.y>max.y) max=max with {y=c.y};
+                                if (c.z>max.z) max=max with {z=c.z};
+                                if (c.w>max.w) max=max with {w=c.w};
+                                space2[c]='#';
+                            }
+                        }
+            return space2;
+        }
+
+        static void Display(Coord min, Coord max, Dictionary<Coord,char> space)
+        {
+            var buf=new StringBuilder(4096);
+            for(int z=min.z;z<=max.z;z++) {
+
+                buf.AppendFormat("z={0}",z);
+                buf.AppendLine();
+                for(int y=min.y;y<=max.y;y++) {
+                    for(int x=min.x;x<=max.x;x++) 
+                        if (space.TryGetValue(new Coord(x,y,z,0), out var c)) buf.Append(c);
+                        else buf.Append('.');
+                    buf.AppendLine();
+                }
+                buf.AppendLine();
+            }
+            Console.WriteLine(buf);
+        }
+    }
+}
